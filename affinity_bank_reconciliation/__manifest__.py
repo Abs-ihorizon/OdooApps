0afc370947346683b49ab3336d55c7b108d7cbfa
Abs-ihorizon <140721005+Abs-ihorizon@users.noreply.github.com>
2025-12-31 15:33:14 +0500

    Add files via upload

---
+++ affinity_bank_reconciliation/__manifest__.py
@@ -0,0 +1,35 @@
+# -- coding: utf-8 --
+
+{
+    'name': 'Bank Reconciliation',
+    'author': 'Affinity Business Suite',
+    'website': 'https://affinitysuite.net',
+    'support': 'info@affinitysuite.net',
+    'category': 'Accounting',
+    'summary': 'Replace default bank statement reconciliation with a manual traditional method',
+    'description': '''This module replaces the default Odoo bank statement reconciliation with a manual approach. Users can reconcile statements line by line by entering dates and verifying balances as per company books and bank.''',
+    'version': '18.0',
+    'depends': ['account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'report/brs_report_template.xml',
+        'report/ir_actions_report.xml',
+        'wizard/bank_statement_views.xml',
+        'wizard/brs_report_wizard_views.xml',
+        'views/account_journal_views.xml',
+        'views/account_move_line_views.xml',
+        'views/account_move_views.xml',
+    ],
+    'assets': {},
+    'images': [
+        'static/description/icon.png',
+        'static/description/banner.png',
+        'static/description/cover.png'
+    ],
+    'price': 60,
+    'currency': 'EUR',
+    'license': 'OPL-1',
+    'application': True,
+    'auto_install': False,
+    'installable': True,
+}
---
+++ affinity_bank_reconciliation/wizard/__init__.py
@@ -0,0 +1,2 @@
+from . import bank_statement
+from . import brs_report_wizard
