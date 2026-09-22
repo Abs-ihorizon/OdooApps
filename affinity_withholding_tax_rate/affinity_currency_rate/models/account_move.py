352bffc9eb81dc32d8a6b1cc147050cce7b3aefe
Abs-ihorizon <140721005+Abs-ihorizon@users.noreply.github.com>
2026-09-22 12:23:43 +0500

    Delete affinity_withholding_tax_rate directory

--- affinity_withholding_tax_rate/affinity_currency_rate/models/account_move.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-
-class AccountMoveInherit(models.Model):
-    _inherit = 'account.move'
-
-    invoice_currency_rate = fields.Float(string='Invoice Currency Rate', digits=(16, 12))
