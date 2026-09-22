c2b503507e373b70860e25b65b55cfef855390f2
Abs-ihorizon <140721005+Abs-ihorizon@users.noreply.github.com>
2026-09-22 18:05:30 +0500

    Update __manifest__.py

--- affinity_gate_pass/__manifest__.py
+++ affinity_gate_pass/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'Affinity Warehouse Gate Pass Management',
+    'name': 'Gate Pass Management',
     'author': 'Affinity Business Suite',
     'website': 'https://affinitysuite.net',
     'support': 'info@affinitysuite.net',
