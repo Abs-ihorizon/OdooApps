a49d38538fe47c7c94ab32c0144d9be7e358f028
Abs-ihorizon <140721005+Abs-ihorizon@users.noreply.github.com>
2026-09-22 15:36:13 +0500

    Delete affinity_pos_pra_integration directory

--- affinity_pos_pra_integration/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import account_move
-from . import pos_config
-from . import pos_order
--- affinity_pos_pra_integration/models/account_move.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-
-class AccountMoveInherit(models.Model):
-    _inherit = 'account.move'
-
-    pra_invoice_number = fields.Char(string='PRA Invoice Number', readonly=True)
