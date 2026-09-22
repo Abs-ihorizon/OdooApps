72d26f38604a6ee0d6aeeb5d8b9520cf73431e2a
Abs-ihorizon <140721005+Abs-ihorizon@users.noreply.github.com>
2026-09-22 18:04:56 +0500

    Update __manifest__.py

--- affinity_withholding_tax/__manifest__.py
+++ affinity_withholding_tax/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'Affinity Withholding Tax and Currency Rate Management',
+    'name': 'Withholding Tax and Currency Rate Management',
     'author': 'Affinity Business Suite',
     'website': 'https://affinitysuite.net',
     'support': 'info@affinitysuite.net',
